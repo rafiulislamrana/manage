04209a67549e4a1da3498fce73a9a669c1b56698
rafiulislamrana <66508638+rafiulislamrana@users.noreply.github.com>
2023-04-25 23:34:44 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
+      // sm: "480px",
+      // md: "768px",
+      // lg: "976px",
+      // xl: "1440px",
     },
     extend: {
       colors: {
